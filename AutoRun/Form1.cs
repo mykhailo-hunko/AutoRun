8134642362307c63cdbb3df9a5d1432083660baa
Михайло Гунько <mykhailo.hunko@nure.ua>
2020-11-16 16:26:10 +0200

    Добавьте файлы проекта.

---
+++ AutoRun/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoRun
+{
+    public partial class Form1 : Form
+    {
+
+        bool isStart = false;
+        public Form1()
+        {
+            InitializeComponent();
+            Program.timers();
+        }
+
+        private void button_start_Click(object sender, EventArgs e)
+        {
+            startJar(null);
+        }
+
+        public void startJar(object y)
+        {
+            if (!isStart)
+            {
+                Process.Start(@"C:\parcer\out\artifacts\parcer_jar\parcer.jar");
+                isStart = true;
+            }
+            else
+            {
+                MessageBox.Show("Не так быстро, будь нежнее. Попробуй через пол часа!");
+            }
+            
+        }
+
+        public void isStartDef(object y)
+        {
+            isStart = false;
+
+        }
+
+      
+    }
+}
---
+++ AutoRun/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoRun
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        /// 
+       static Form1 form;
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            form = new Form1();
+            Application.Run(form);
+        }
+       public static void timers()
+        {
+            TimerCallback tm = new TimerCallback(form.startJar);
+            // создаем таймер
+            System.Threading.Timer timer = new System.Threading.Timer(tm, null, 0, 1000*30*60);
+
+            TimerCallback tm2 = new TimerCallback(form.isStartDef);
+            // создаем таймер
+            System.Threading.Timer timer2 = new System.Threading.Timer(tm2, null, 30*1000, 1000*12*60*60);
+        }
+
+    }
+}
